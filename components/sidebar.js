0223b444ae846de882475a0982561e810f9c13be
radkardhanu <radkardhanshri@gmail.com>
2022-07-22 21:11:50 +0530

    page

---
+++ components/sidebar.js
@@ -0,0 +1,44 @@
+d
+// let navbar=()=>{
+//     return `<div id="navbar">
+//     <h1><a href="fooditems.html">Food-Items</a></h1>
+//     <div id="options">
+//         <h2><a href="random.html">Respie-of-Day</a></h2>
+//         <h2><a href="choose.html">Search-Food</a></h2>
+//         <h2><a href="signup.html">Sign-up</a></h2>
+//         <h2><a href="login.html">Login</a></h2>
+//     </div>
+// </div>`;
+// }
+// export default navbar;
+
+function sidebar() {
+    return ` <div id="navbar">
+    <ul id=sid>
+
+       <li><a  href="">Categories</a></li>
+
+       <li><a href="">Brands</a>
+        <div id=brand">
+          <ul id="brand1">
+            <div id="brand2">
+               <li><a href="">search</a></li>
+        </div>
+          </ul>
+       </li>
+
+       <li><a href="">Beauty Advice</a></li>
+       <li><a href="">Luxe</a></li>
+       <li><a href="">Nykaa Fashion</a></li>
+    
+   </ul>
+</div>
+    `;
+  
+  }
+
+  export default sidebar;
+  
+  
+
+  
