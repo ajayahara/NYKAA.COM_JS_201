8babd16fd077dddf77d480af44148e7512f88684
ajayahara <abak00350@gmail.com>
2022-07-23 09:03:53 +0530

    All comlete

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+let bt1=document.getElementById("firstb")
+bt1.addEventListener("click",function(){
+   window.location.href="home.html";//need to update
+})
+let bt2=document.getElementById("secondb");
+bt2.addEventListener("click",function(){
+   window.location.href="signup.html";
+})
